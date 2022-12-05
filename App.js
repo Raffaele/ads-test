2e0dd6cb3b845c7c79ea1400ff53d6209ab40e0d
Raffaele <mori.raffaele@gmail.com>
2022-12-05 20:40:46 +0000

    Added dependencies

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import mobileAds from 'react-native-google-mobile-ads';
+
+// mobileAds()
+//   .initialize()
+
+// https://docs.page/invertase/react-native-google-mobile-ads documentation
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!?</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
